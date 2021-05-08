ef83be7d91ae4987ca425b8f9e50be939be786d8
Joshua Richards <joshua.j.richards@gmail.com>
2021-05-08 15:49:21 -0400

    testing filters

---
+++ filter.js
@@ -0,0 +1,18 @@
+const data = {
+  countries: [],
+  cities: [],
+  salary: [],
+};
+
+const loadData = (data) => {};
+
+const openMenu = () => {};
+
+const closeMenu = () => {};
+
+const handleFilterClick = () => {};
+
+const handleFilterBack = () => {};
+
+//initialize filter
+loadData(data);
